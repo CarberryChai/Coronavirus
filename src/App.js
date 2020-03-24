2605dc8e88b37faccbc952e7de4a60957a6b5740
CarberryChai <CarberryChai@gmail.com>
2020-03-24 17:48:48 +0800

    update api

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useMemo } from 'react'
 import './App.css'
 import DataCard from './components/Header/DataCard'
 import request from './util/request'
-import { Select, Row, Col } from 'antd'
+import { Select, Row, Col, Empty } from 'antd'
 const { Option } = Select
 function App() {
-  const [country, setCountryList] = useState(null)
+  const [countryList, setCountryList] = useState([])
   const [singleCountry, setSingleCountry] = useState('China')
   useEffect(() => {
     request('https://covid19.mathdro.id/api/countries').then(res => {
-      setCountryList(res)
+      setCountryList(res.countries)
     })
   }, [])
   const handleChange = value => {
     setSingleCountry(value)
   }
+  const short = useMemo(() => {
+    const temp = countryList.find(item => item.name === singleCountry)
+    return temp && temp.iso3 ? temp.iso3 : ''
+  }, [singleCountry, countryList])
   return (
     <div className='App'>
       <h1 style={{ marginBottom: 50 }}>Global Coronavirus Disease Data</h1>
@@ -31,24 +35,21 @@ function App() {
               opt.children.toLowerCase().startsWith(input.toLowerCase())
             }
           >
-            {country &&
-              Object.entries(country.countries).map(
-                ([country, code], index) => (
-                  <Option value={country} key={code + index}>
-                    {country}
-                  </Option>
-                )
-              )}
+            {countryList.map((country, index) => (
+              <Option value={country.name} key={index}>
+                {country.name}
+              </Option>
+            ))}
           </Select>
         </Col>
         <Col span={12}>{singleCountry}</Col>
       </Row>
-      {country && (
+      {short ? (
         <DataCard
-          url={`https://covid19.mathdro.id/api/countries/${
-            country.iso3[country.countries[singleCountry]]
-          }`}
+          url={`https://covid19.mathdro.id/api/countries/${short}`}
         ></DataCard>
+      ) : (
+        <Empty />
       )}
     </div>
   )
